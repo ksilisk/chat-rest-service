1cfc4013547862b8b3aecd2a2c946fa08d4d169f
Shaliko Salimov <shaliko9@yandex.ru>
2023-05-02 23:24:47 +0300

    Create table repositories

---
+++ src/main/java/com/ksilisk/chatservice/repository/ChatRepository.java
@@ -0,0 +1,12 @@
+package com.ksilisk.chatservice.repository;
+
+import com.ksilisk.chatservice.entity.Chat;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ChatRepository extends CrudRepository<Chat, Long> {
+    Optional<Chat> findChatById(long id);
+}
