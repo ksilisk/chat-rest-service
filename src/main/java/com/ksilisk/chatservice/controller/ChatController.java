1fe4c5724e58f3d7fe8974d2532b4f9c1b9a5f2f
Shaliko Salimov <shaliko9@yandex.ru>
2023-05-03 23:38:24 +0300

    Create RestControllers

---
+++ src/main/java/com/ksilisk/chatservice/controller/ChatController.java
@@ -0,0 +1,9 @@
+package com.ksilisk.chatservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/chats")
+public class ChatController {
+}
---
+++ src/main/java/com/ksilisk/chatservice/controller/RegistrationController.java
@@ -0,0 +1,7 @@
+package com.ksilisk.chatservice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+}
