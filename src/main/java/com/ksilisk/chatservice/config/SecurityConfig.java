be73e11e6cb00d6d3f52daf4f9dd93d7cad6a2cb
Shaliko Salimov <shaliko9@yandex.ru>
2023-06-03 18:04:47 +0300

    Add Security Configs

--- src/main/java/com/ksilisk/chatservice/config/SecurityConfig.java
+++ src/main/java/com/ksilisk/chatservice/config/SecurityConfig.java
@@ -3,9 +3,15 @@ package com.ksilisk.chatservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +20,19 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests()
+                .requestMatchers("/auth").permitAll()
+                .requestMatchers("/register").permitAll()
+                .anyRequest().authenticated();
+        return http.build();
     }
 }
--- src/main/java/com/ksilisk/chatservice/controller/ChatController.java
+++ src/main/java/com/ksilisk/chatservice/controller/ChatController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/chats")
+@RequestMapping("/chats")
 public class ChatController {
 }
